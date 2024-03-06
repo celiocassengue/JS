6d6479e29ee237a45f2068310ed7888a4e951cd8
celiocassengue <celiocassengue79@gmail.com>
2024-03-06 09:40:49 +0000

    Initial commit

---
+++ exercicios/Hora do Dia/script.js
@@ -0,0 +1,26 @@
+function carregar(){
+  var msg=document.getElementById("msg")
+var img=document.getElementById("img")
+  var manha=document.getElementById("imagem")
+  var data=new Date
+  var hora=data.getHours()
+  var minuto=data.getMinutes()
+  var segundo=data.getSeconds()
+  msg.innerHTML="Agora são: "+hora+"h"+":"+minuto+"min"+":"+segundo+"seg"
+
+if(hora>=0 && hora <12){
+  //BOM DIA
+  manha.src="fotomanha.png"
+  document.body.style.background="#8eb1d6"
+}
+else if(hora >=12 && hora <18){
+  //BOA TARDE
+  manha.src="fototarde.png"
+  document.body.style.background="#ce6767"
+}
+else{//BOA NOITE
+  manha.src="fotonoite.png"
+  document.body.style.background="#11252b"
+}
+}
+carregar()
