bda947e516950e1fd034302aee95eee0fba890c1
celiocassengue <celiocassengue79@gmail.com>
2024-04-15 20:38:05 +0000

    atualização

--- modulo D/repetição.js
+++ modulo D/repetição.js
@@ -1,5 +1,20 @@
-var  m = 1
-while(m<=10){
-    console.log("Um mano não é um mano sem seus  manos!")
-    m++
+/* 
+var  m = 1 
+var n = 1 
+while(m&&n <=10){
+    conso le.log(n+" "+"Um mano não  é um mano sem seus  manos!")
+     m++
+     n++
+} 
+*/ 
+
+var c=1
+/*
+while(c<=5){
+    console.log(`${c}`)
+    c++
+}
+*/
+for(c=1; c<=5; c++){
+    console.log(`${c}`)
 }
